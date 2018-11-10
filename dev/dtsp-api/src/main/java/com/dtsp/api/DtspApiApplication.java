7127ea3774718a5cfb4cab777cb70510f95ae6f8
dimomass <dimitar.dimitrov@aossia.com>
2018-11-10 11:07:51 +0200

    Initial commit

---
+++ dev/dtsp-api/src/main/java/com/dtsp/api/DtspApiApplication.java
@@ -0,0 +1,12 @@
+package com.dtsp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtspApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtspApiApplication.class, args);
+	}
+}
